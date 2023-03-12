868e7f3c3848aeb62029d327da495c937ed25585
NaddaMhmd <105833351+NaddaMhmd@users.noreply.github.com>
2023-03-13 00:01:29 +0300

    Update models.py

--- On_the_Automatic_Generation_of_Medical_Imaging_Reports/models.py
+++ On_the_Automatic_Generation_of_Medical_Imaging_Reports/models.py
@@ -1,7 +1,7 @@
 
 import os
 os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"]="3"
+os.environ["CUDA_VISIBLE_DEVICES"]="0"
 import torch
 import torch.nn as nn
 import torchvision.models as models
